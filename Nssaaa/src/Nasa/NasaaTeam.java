37bd08a87db51dbbf542d17a313de6e463026fe2
Nasaa <nabunaska@gmail.com>
2024-04-03 10:36:23 +0800

    Commit Create

---
+++ Nssaaa/src/Nasa/NasaaTeam.java
@@ -0,0 +1,11 @@
+package Nasa;
+
+public class NasaaTeam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("This iS AIDHHH!!!!!!");
+	System.out.println("WE ARE STRONG BECAUSE WE ARE TOGETHER!!!!!!!!!!!!!!!");
+	}
+
+}
