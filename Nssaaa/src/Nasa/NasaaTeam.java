5903c92cee595b994f1db4682eb72138c7149dc0
bn <baysaa>
2024-04-03 10:53:51 +0800

    Baysaa

--- Nssaaa/src/Nasa/NasaaTeam.java
+++ Nssaaa/src/Nasa/NasaaTeam.java
@@ -5,7 +5,8 @@ public class NasaaTeam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("This iS AIDHHH!!!!!!");
-	System.out.println("WE ARE STRONG BECAUSE WE ARE TOGETHER!!!!!!!!!!!!!!!");
+	System.out.println("WE ARE  BECAUSE WE ARE TOGETHER!!!!!!!!!!!!!!!");
+	r
 	}
 
 }
